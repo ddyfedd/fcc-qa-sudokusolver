d723a7934d9a90d53db08ea7e2a016d9a91f3404
ddyfedd <dzsamboki.techwriter@gmail.com>
2021-04-10 17:39:29 +0200

    random commit

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -16,65 +16,50 @@ class SudokuSolver {
   }
 
   solveSudoku(grid, row, col) {
-    if (row == 9 - 1 && col == 9) {
-      return true;
-    }
+    if (row == 9 - 1 && col == 9) return grid;
+    
     if (col == 9) {
       row++;
       col = 0;
     }
 
-    if (grid[row][col] != 0) {
-      return solveSudoku(grid, row, col + 1);
-    }
+    if (grid[row][col] != 0) return this.solveSudoku(grid, row, col + 1);
 
     for (let num = 1; num < 10; num++) {
-      if (isSafe(grid, row, col, num)) {
+      if (this.isSafe(grid, row, col, num)) {
         grid[row][col] = num;
-        if (solveSudoku(grid, row, col +1)) {
-          return true;
-        }
-      grid[row][col] = 0;
+        if (this.solveSudoku(grid, row, col +1)) return grid;
       }
-    return false;
+      grid[row][col] = 0;
     }
+    return false;
   }
 
   isSafe(grid, row, col, num) {
-    for (let x = 0; x <= 8; x++) {
-      if(grid[row][x] == num) {
-        return false;
-      }
-    }
-    for (let x = 0; x <= 8; x++) {
-      if(grid[x][col] == num) {
-        return false;
-      }
-    }
+    for (let x = 0; x <= 8; x++) if(grid[row][x] == num) return false;
+    
+    for (let x = 0; x <= 8; x++) if(grid[x][col] == num) return false;
 
     let startRow = row - (row % 3),
       startCol = col - (col % 3);
-    for (let i = 0; i < 3; i++) {
-      for (let j = 0; j < 3; j++) {
-        if(grid[i+ startRow][j + startCol] == num) {
-          return false;
-        }
-      }
-    }
-    return true;
+    for (let i = 0; i < 3; i++) 
+      for (let j = 0; j < 3; j++) 
+        if(grid[i + startRow][j + startCol] == num) return false;
+    
+    return grid;
   }
   
   transform(puzzleString) {
     let grid = [
-      [0, 0, 0, 0, 0, 0, 0, 0, 0,],
-      [0, 0, 0, 0, 0, 0, 0, 0, 0,],
-      [0, 0, 0, 0, 0, 0, 0, 0, 0,],
-      [0, 0, 0, 0, 0, 0, 0, 0, 0,],
-      [0, 0, 0, 0, 0, 0, 0, 0, 0,],
-      [0, 0, 0, 0, 0, 0, 0, 0, 0,],
-      [0, 0, 0, 0, 0, 0, 0, 0, 0,],
-      [0, 0, 0, 0, 0, 0, 0, 0, 0,],
-      [0, 0, 0, 0, 0, 0, 0, 0, 0,],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
     ];
     let row = -1;
     let col = 0;
