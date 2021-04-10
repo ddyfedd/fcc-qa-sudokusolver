700acefb739165b6f65d01773a105dd5693391de
ddyfedd <dzsamboki.techwriter@gmail.com>
2021-04-10 18:40:36 +0200

    don't know why not working

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -3,16 +3,76 @@ class SudokuSolver {
   validate(puzzleString) {
   }
 
+  letterToNumber(row) {
+    switch (row.toUpperCase()) {
+      case 'A':
+        return 1;
+      case 'B':
+        return 2;
+      case 'C':
+        return 3;
+      case 'D':
+        return 4;
+      case 'E':
+        return 5;
+      case 'F':
+        return 6;
+      case 'G':
+        return 7;
+      case 'H':
+        return 8;
+      case 'I':
+        return 9;
+      default:
+        return 'none';
+    }
+  }
+  
   checkRowPlacement(puzzleString, row, column, value) {
+    let grid = this.transform(puzzleString);
+    row = this.letterToNumber(row);
 
+    if (grid[row - 1][column - 1] !== 0) {
+      return false;
+    }
+    for (let i = 0; i < 9; i++) {
+      if (grid[row -1][i] == value) {
+        return false;
+      }
+    }
+    return true;
   }
 
   checkColPlacement(puzzleString, row, column, value) {
+    let grid = this.transform(puzzleString);
+    row = this.letterToNumber(row);
 
+    if (grid[row - 1][column - 1] !== 0) {
+      return false;
+    }
+    for (let i = 0; i < 9; i++) {
+      if (grid[i][column - 1] == value) {
+        return false;
+      }
+    }
+    return true;
   }
 
-  checkRegionPlacement(puzzleString, row, column, value) {
+  checkRegionPlacement(puzzleString, row, col, value) {
+    let grid = this.transform(puzzleString);
+    row = this.letterToNumber(row);
 
+    if (grid[row - 1][column - 1] !== 0) {
+      return false;
+    }
+    let startRow = row - row % 3;
+    let startCol = col - col % 3;
+    
+    for (let i = 0; i < 3; i++)
+      for (let j = 0; j < 3; j++)
+        if (grid[i + startRow][j + startCol] == num) return false;
+
+    return true;
   }
 
   solveSudoku(grid, row, col) {
