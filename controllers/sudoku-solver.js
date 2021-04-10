007b3623bc8f72b57b19f63c19d5edd79c72bc94
ddyfedd <dzsamboki.techwriter@gmail.com>
2021-04-10 18:05:16 +0200

    format two functions

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -17,24 +17,19 @@ class SudokuSolver {
 
   solveSudoku(grid, row, col) {
 
-    if (row == 9 - 1 && col == 9)
-      return true;
-
+    if (row == 9 - 1 && col == 9) return true;
 
     if (col == 9) {
       row++;
       col = 0;
     }
 
+    if (grid[row][col] != 0) return this.solveSudoku(grid, row, col + 1);
 
-    if (grid[row][col] != 0)
-      return this.solveSudoku(grid, row, col + 1);
-
-    for (num = 1; num < 10; num++) {
+    for (let num = 1; num < 10; num++) {
       if (isSafe(grid, row, col, num)) {
         grid[row][col] = num;
-        if (this.solveSudoku(grid, row, col + 1))
-        return true;
+        if (this.solveSudoku(grid, row, col + 1)) return true;
       }
     grid[row][col] = 0;
     }
@@ -43,24 +38,20 @@ class SudokuSolver {
 
   isSafe(grid, row, col, num) {
 
-
-    for (x = 0; x <= 8; x++)
-      if (grid[row][x] == num)
-      return false;
+    for (let x = 0; x <= 8; x++)
+      if (grid[row][x] == num) return false;
 
 
-    for (x = 0; x <= 8; x++)
-      if (grid[x][col] == num)
-      return false;
+    for (let x = 0; x <= 8; x++)
+      if (grid[x][col] == num) return false;
 
 
-    startRow = row - row % 3;
-    startCol = col - col % 3;
+    let startRow = row - row % 3;
+    let startCol = col - col % 3;
     
-    for (i = 0; i < 3; i++)
-      for (j = 0; j < 3; j++)
-        if (grid[i + startRow][j + startCol] == num)
-        return false;
+    for (let i = 0; i < 3; i++)
+      for (let j = 0; j < 3; j++)
+        if (grid[i + startRow][j + startCol] == num) return false;
 
     return true;
 }
