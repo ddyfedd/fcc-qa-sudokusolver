3768a3abd4cc69a3a58afdeff8a3dfd32d0b992d
ddyfedd <dzsamboki.techwriter@gmail.com>
2021-04-10 18:14:57 +0200

    elvileg jó

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -17,7 +17,7 @@ class SudokuSolver {
 
   solveSudoku(grid, row, col) {
 
-    if (row == 9 - 1 && col == 9) return true;
+    if (row == 9 - 1 && col == 9) return grid;
 
     if (col == 9) {
       row++;
@@ -27,9 +27,9 @@ class SudokuSolver {
     if (grid[row][col] != 0) return this.solveSudoku(grid, row, col + 1);
 
     for (let num = 1; num < 10; num++) {
-      if (isSafe(grid, row, col, num)) {
+      if (this.isSafe(grid, row, col, num)) {
         grid[row][col] = num;
-        if (this.solveSudoku(grid, row, col + 1)) return true;
+        if (this.solveSudoku(grid, row, col + 1)) return grid;
       }
     grid[row][col] = 0;
     }
@@ -54,10 +54,53 @@ class SudokuSolver {
         if (grid[i + startRow][j + startCol] == num) return false;
 
     return true;
-}
+  }
+
+  transform(puzzleString) {
+    let grid = [
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    ];
+
+    let row = -1;
+    let col = 0;
+
+    for (let i = 0; i < puzzleString.length; i++) {
+      if (i % 9 == 0) {
+        row++;
+      }
+      if (col % 9 == 0) {
+        col = 0;
+      }
+
+      grid[row][col] = puzzleString[i] === '.' ? 0 : +puzzleString[i];
+      col++;
+    }
+    return grid;
+  }
+
+  transformBack(grid) {
+    return grid.flat().join('');
+  }
 
   solve(puzzleString) {
-    
+    let grid = this.transform(puzzleString);
+    let solved = this.solveSudoku(grid, 0, 0);
+
+    if (!solved) {
+      return false;
+    }
+
+    let solvedString = this.transformBack(solved);
+    console.log('solvedString :>> ', solvedString);
+    return solvedString;
   }
 }
 
